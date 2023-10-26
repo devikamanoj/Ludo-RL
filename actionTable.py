da5384f38da605d6bfa2dbaa9d6a025a51e05884
Devika Manoj <dmenon138@gmail.com>
2023-10-26 10:40:18 +0530

    comments added

--- actionTable.py
+++ actionTable.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 
-class ActionTableEntry():
+class ActionTableEntry(): #used to store the action table entries for a given state and action pair 
     def __init__(self, piece, value):
         super().__init__()
-        self.pice = piece
-        self.value = value
+        self.pice = piece #piece to move
+        self.value = value #value of the action
 
-    def add_entry(self, piece, value):
+    def add_entry(self, piece, value): #add an entry to the action table
         self.__piece.append(piece)
         self.__value.append(value)
 
@@ -16,29 +16,32 @@ class ActionTable():
     action_table = None
     state = 0
 
-    def __init__(self, states, actions):
+    def __init__(self, states, actions):# defines constructor method for action table
         super().__init__()
-        self.states = states
-        self.actions = actions
+        self.states = states #number of states
+        self.actions = actions #number of actions
         self.reset()
 
-    def set_state(self, state):
+    def set_state(self, state): #set the current state of the action table
         self.state = state.value
 
-    def get_action_table(self):
+    def get_action_table(self): #returns the action table(numpy array that stores the action table)
         return self.action_table
 
     def get_piece_to_move(self, state, action):
+        #returns the piece to move for the given state and action
+        #returns -1 if the state or action is invalid
         if state < 0 or action < 0:
             return -1
         return int(self.piece_to_move[state, action])
 
-    def reset(self):
+    def reset(self): #returns table to the initial state (all values are set to nan)
         self.action_table = np.full((self.states, self.actions), np.nan)
         self.piece_to_move = np.full((self.states, self.actions), np.nan)
 
-    # todo: piece implementation missing...
+    # update the action table with the given action and piece to move
     def update_action_table(self, action, piece, value):
-        if np.isnan(self.action_table[self.state, action.value]):
+        if np.isnan(self.action_table[self.state, action.value]): 
+            #checks if the action table entry for the given state and action is NaN. If it is, then the action table entry has not yet been initialized and needs to be initialized.
             self.action_table[self.state, action.value] = 1
             self.piece_to_move[self.state, action.value] = piece
